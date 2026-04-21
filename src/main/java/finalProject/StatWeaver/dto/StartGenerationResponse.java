172cbf9188f54e75a9c73c7f889823f23d7e041a
aysoobi <dnmlion442@gmail.com>
2026-04-21 20:24:19 +0500

    Update StartGenerationResponse.java

--- src/main/java/finalProject/StatWeaver/dto/StartGenerationResponse.java
+++ src/main/java/finalProject/StatWeaver/dto/StartGenerationResponse.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 @Data
 public class StartGenerationResponse {
-    //для того чтобы кол-во вопросов выбирать
     private List<QuestionDto> questions;
     private int questionCount;
 }
