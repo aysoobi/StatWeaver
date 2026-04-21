c17dc3a7a3311e3f0118fcb9739fcb0b064ca5c9
aysoobi <dnmlion442@gmail.com>
2026-04-21 20:22:57 +0500

    Update CreatePageController.java

--- src/main/java/finalProject/StatWeaver/controller/CreatePageController.java
+++ src/main/java/finalProject/StatWeaver/controller/CreatePageController.java
@@ -14,13 +14,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Controller
+    @RequiredArgsConstructor
 public class CreatePageController {
 
     private final CharacterGenerationService characterGenerationService;
 
-    public CreatePageController(CharacterGenerationService characterGenerationService) {
-        this.characterGenerationService = characterGenerationService;
-    }
 
     @GetMapping("/create")
     public String createStartPage(
@@ -56,19 +54,16 @@ public class CreatePageController {
             @RequestParam Map<String, String> params,
             Model model
     ) {
-        //c фронта мы берем ответ_и айдишка, а это чтобы его на
         try {
             Map<Long, Long> selectedAnswers = new HashMap<>();
-            for (Map.Entry<String, String> entry : params.entrySet()) {//чтобы получить одним списком ключ-пары
+            for (Map.Entry<String, String> entry : params.entrySet()) {
                 String key = entry.getKey();
                 if (!key.startsWith("answer_")) {
                     continue;
                 }
                 long questionId = Long.parseLong(key.substring("answer_".length()));
-                //берет только айдишку
-                //переводит его в лонг
+            
                 long answerId = Long.parseLong(entry.getValue());
-                //пэйрит айдишку вопроса и ответа
                 selectedAnswers.put(questionId, answerId);
             }
 
